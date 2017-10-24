d2d8118d9476f2a7feb286f29444ecdb2bad4bdd
Jakharbek <javhar_work@mail.ru>
2017-10-24 23:30:59 +0500

    edit

---
+++ src/AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace jakharbek\user;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "H2el123lo!";
+    }
+}
---
+++ src/Bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace jakharbek\user;
+
+use Yii;
+use yii\base\BootstrapInterface;
+
+class Bootstrap implements BootstrapInterface{
+    public function bootstrap($app)
+    {
+        //
+    }
+}
